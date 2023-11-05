4740de46159a85c4f892bc7968184f75f5fffdb2
Christian Kröger <c.kroeger@reply.de>
2023-11-05 18:04:02 +0100

    add custom object mapper reconfigure generator

--- src/main/java/com/github/ckroeger/openapi/config/AppConfig.java
+++ src/main/java/com/github/ckroeger/openapi/config/AppConfig.java
@@ -4,8 +4,11 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.servers.Server;
+import org.openapitools.jackson.nullable.JsonNullableModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
 import java.util.List;
 
@@ -18,11 +21,19 @@ public class AppConfig {
     public OpenAPI springUserOpenAPI() {
         return new OpenAPI()
                 .info(new Info()
-                        .title("QRService API")
+                        .title("Petstore API")
                         .description("The available REST-API to create qr-codes.")
                         .license(new License()
                                 .name("MIT License")
                                 .url("http://www.opensource.org/licenses/mit-license.php")))
                 .servers(List.of(new Server().url("/").description(serverDescription)));
+    }
+
+    @Bean
+    @Primary
+    public Jackson2ObjectMapperBuilder customObjectMapper() {
+        return new Jackson2ObjectMapperBuilder()
+                // other configs are possible
+                .modules(new JsonNullableModule());
     }
 }
